4d1bea374d1e80d82af811d854ea0cb4c6cf239c
Tommi Latti <tlatti@fastly.com>
2021-08-10 15:43:47 +0900

    integrate role policy assignments with creation

--- project2/tf/main.tf
+++ project2/tf/main.tf
@@ -1,11 +1,12 @@
-# create the bucket
 provider "aws" {
 	region = var.aws_region
 }
 
-resource "aws_s3_bucket" "a" {
-  bucket = "tl-incidents2"
-	acl = var.acl
+# create the bucket
+resource "aws_s3_bucket" "incidents_bucket" {
+  bucket = "tl-incidents"
+  acl = var.acl
+	force_destroy = true	
 
 	versioning {
 	  enabled = var.versioning
@@ -16,12 +17,13 @@ resource "aws_s3_bucket" "a" {
 
 # upload test data to the bucket
 resource "aws_s3_bucket_object" "testdata" {
-	bucket = aws_s3_bucket.a.id
+	bucket = aws_s3_bucket.incidents_bucket.id
 	key = "example.json"
 	acl = var.acl
 	source = "example.json"
 }
 
+# assign EKS to role
 data "aws_iam_policy_document" "k8_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -33,10 +35,27 @@ data "aws_iam_policy_document" "k8_assume_role_policy" {
   }
 }
 
+# create role for EKS
 resource "aws_iam_role" "kuber_service_role" {
 	name = "kuberole"
 	assume_role_policy = data.aws_iam_policy_document.k8_assume_role_policy.json
+	managed_policy_arns = [
+		"arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+		"arn:aws:iam::aws:policy/AmazonS3FullAccess"
+		]
 }
+
+## assign s3 to role
+#resource "aws_iam_role_policy_attachment" "kuberole-eks" {
+#  role       = aws_iam_role.kuber_service_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+#}
+
+# assign EKSCluster policy to role
+#resource "aws_iam_role_policy_attachment" "kuberole-eks" {
+#  role       = aws_iam_role.kuber_service_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#}
 
 # resource "aws_iam_role_policy_attachment" "attach" {
 #	role = aws_iam_role.kuberole
