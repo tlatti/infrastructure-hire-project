2a68d5d2e0edb9d02c06d83af5296486c628a4cd
Tommi Latti <tlatti@fastly.com>
2021-08-23 19:03:44 +0900

    linted the main files for standardisation

--- project2/tf/variables.tf
+++ project2/tf/variables.tf
@@ -1,39 +1,39 @@
 variable "aws_region" {
-  type = string
-	description = "The AWS region"
-  default = "us-east-1"
+  type        = string
+  description = "The AWS region"
+  default     = "us-east-1"
 }
 
 variable "bucket_prefix" {
-  type = string
+  type        = string
   description = "We need globally unique bucket names"
-  default = "tl-"
+  default     = "tl-"
 }
 
 variable "tags" {
-  type = map
+  type        = map(any)
   description = "Tags for security groups"
   default = {
     environment = "prod"
-    terraform = "true"
+    terraform   = "true"
   }
 }
 
 variable "versioning" {
-  type = bool
+  type        = bool
   description = "Do we need versioning"
-  default = false
+  default     = false
 }
 
 variable "acl" {
-  type = string
+  type        = string
   description = "Set default to private"
-  default = "private"
+  default     = "private"
 }
 
 variable "cluster_name" {
-  type = string
+  type        = string
   description = "EKS cluster name"
-  default = "tl-eks-cluster"
+  default     = "tl-eks-cluster"
 }
 
